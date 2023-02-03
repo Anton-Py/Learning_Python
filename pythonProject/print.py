a4bd4cd95d8396cc0ba946b194ac6bede196376d
Anton <a.lyakh@gmail.com>
2023-02-04 03:07:53 +0700

    fix gitignore

---
+++ pythonProject/print.py
@@ -0,0 +1,18 @@
+print('"30" августа 1903г.')
+
+print('C:\\Windows\\System32\\Drivers\\etc\\hosts')
+
+a = 4
+b = 5
+c = a + b
+print("Результат =", c)
+
+string = "String1\
+String2"
+print(string)
+
+string = r"строка1\nтекст\\"
+print(string)
+
+string = "ab" * 3
+print(string)
