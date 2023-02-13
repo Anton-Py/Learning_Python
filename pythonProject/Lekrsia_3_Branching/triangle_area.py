e45e63244f5f3a94f0f80c2893fc8a991c2908bd
Anton <a.lyakh@gmail.com>
2023-02-14 04:21:54 +0700

    fix homework

--- pythonProject/Lekrsia_3_Branching/triangle_area.py
+++ pythonProject/Lekrsia_3_Branching/triangle_area.py
@@ -1,15 +1,19 @@
 import math
 
-x1, y1 = float(input("Введите координату x1: ")), float(input("Введите координату y1: "))
-x2, y2 = float(input("Введите координату x2: ")), float(input("Введите координату y2: "))
-x3, y3 = float(input("Введите координату x3: ")), float(input("Введите координату y3: "))
+x_1 = float(input("Введите координату x1: "))
+y_1 = float(input("Введите координату y1: "))
+x_2 = float(input("Введите координату x2: "))
+y_2 = float(input("Введите координату y2: "))
+x_3 = float(input("Введите координату x3: "))
+y_3 = float(input("Введите координату y3: "))
 
-if 0.5 * ((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3)) == 0:
+if abs((0.5 * ((x_1 - x_3) * (y_2 - y_3) - (x_2 - x_3) * (y_1 - y_3)) - 0)) <= 0.1:
     print("Точки лежат на одной прямой, площадь треугольника вычисляться не будет")
 else:
-    a = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-    b = math.sqrt((x1 - x3) ** 2 + (y1 - y3) ** 2)
-    c = math.sqrt((x3 - x2) ** 2 + (y3 - y2) ** 2)
+    a = math.sqrt((x_1 - x_2) ** 2 + (y_1 - y_2) ** 2)
+    b = math.sqrt((x_1 - x_3) ** 2 + (y_1 - y_3) ** 2)
+    c = math.sqrt((x_3 - x_2) ** 2 + (y_3 - y_2) ** 2)
+
     semi_perimeter = (a + b + c) * 0.5
     triangle_area = math.sqrt(semi_perimeter * (semi_perimeter - a) * (semi_perimeter - b) * (semi_perimeter - c))
     print("Area of a triangle:", triangle_area)
--- pythonProject/Lektsia_2_Python_Stroki_Arifmetika_Chtenie_s_konsoli/apartments.py
+++ pythonProject/Lektsia_2_Python_Stroki_Arifmetika_Chtenie_s_konsoli/apartments.py
@@ -1,17 +1,20 @@
-floors_quantity_in_house = int(input("Введите количество этажей: "))
-entrances_quantity_in_house = int(input("Введите количество подъездов: "))
-apartment_quantity = int(input("Введите квартиру: "))
+floors_quantity = int(input("Введите количество этажей: "))
+entrances_quantity = int(input("Введите количество подъездов: "))
+apartment_amount = int(input("Введите квартиру: "))
+
 apartments_quantity_on_floor = 4
 
-if (floors_quantity_in_house * entrances_quantity_in_house * apartments_quantity_on_floor) < apartment_quantity:
+if (floors_quantity * entrances_quantity * apartments_quantity_on_floor) < apartment_amount:
     print("Квартиры с таким номером нет в доме")
 else:
-    entrance_number = (apartment_quantity - 1) // (floors_quantity_in_house * 4) + 1
+    entrance_number = (apartment_amount - 1) // (floors_quantity * apartments_quantity_on_floor) + 1
     print(f"Номер подъезда: {entrance_number}")
-    floor_in_entrance = (apartment_quantity - 1) % (floors_quantity_in_house * 4) // 4 + 1
-    print(f"Номер этажа: {floor_in_entrance}")
 
-    apartment_location = apartment_quantity % apartments_quantity_on_floor
+    floor_number = \
+        (apartment_amount - 1) % floors_quantity * apartments_quantity_on_floor // apartments_quantity_on_floor + 1
+    print(f"Номер этажа: {floor_number}")
+
+    apartment_location = apartment_amount % apartments_quantity_on_floor
 
     if apartment_location == 1:
         print(f"Квартира ближняя слева")
