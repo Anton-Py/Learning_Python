140ae27387932924f5b6f0e8b0396486cfaf7497
Anton <a.lyakh@gmail.com>
2023-02-15 01:00:04 +0700

    fix age

--- pythonProject/Lekrsia_3_Branching/age.py
+++ pythonProject/Lekrsia_3_Branching/age.py
@@ -1,16 +1,17 @@
 age = int(input("Введите свой возраст от 1 до 112 включительно: "))
 
+remainder_years_division = age % 10
+
 if age < 1:
     print("Вы слишком малы")
 elif age > 112:
     print("Вы слишком стары")
-else:
-    if 11 <= age <= 14 or (age // 10 == (11 or 12 or 13)):
-        print("лет")
-    elif 2 <= age % 10 <= 4:
-        print("года")
-    elif age % 10 == 1:
-        print("год")
-    else:
-        print("лет")
 
+if 11 <= age <= 14 or (age // 10 == (11 or 12 or 13)):
+    print("Вам {} лет".format(age))
+elif 2 <= remainder_years_division <= 4:
+    print("Вам {} года".format(age))
+elif remainder_years_division == 1:
+    print("Вам {} год".format(age))
+else:
+    print("Вам {} лет".format(age))
