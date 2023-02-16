ea5c0ee16fdedd90296bcd3bd01db1fe4b61447a
Anton <a.lyakh@gmail.com>
2023-02-17 02:32:14 +0700

    fix homework

--- pythonProject/Lektsia_2_Python_Stroki_Arifmetika_Chtenie_s_konsoli/apartments.py
+++ pythonProject/Lektsia_2_Python_Stroki_Arifmetika_Chtenie_s_konsoli/apartments.py
@@ -1,20 +1,20 @@
 floors_quantity = int(input("Введите количество этажей: "))
 entrances_quantity = int(input("Введите количество подъездов: "))
-apartment_amount = int(input("Введите квартиру: "))
+apartment_number = int(input("Введите квартиру: "))
 
 apartments_quantity_on_floor = 4
 
-if (floors_quantity * entrances_quantity * apartments_quantity_on_floor) < apartment_amount:
+if (floors_quantity * entrances_quantity * apartments_quantity_on_floor) < apartment_number:
     print("Квартиры с таким номером нет в доме")
 else:
-    entrance_number = (apartment_amount - 1) // (floors_quantity * apartments_quantity_on_floor) + 1
+    entrance_number = (apartment_number - 1) // (floors_quantity * apartments_quantity_on_floor) + 1
     print(f"Номер подъезда: {entrance_number}")
 
     floor_number = \
-        (apartment_amount - 1) % floors_quantity * apartments_quantity_on_floor // apartments_quantity_on_floor + 1
+        (apartment_number - 1) % floors_quantity * apartments_quantity_on_floor // apartments_quantity_on_floor + 1
     print(f"Номер этажа: {floor_number}")
 
-    apartment_location = apartment_amount % apartments_quantity_on_floor
+    apartment_location = apartment_number % apartments_quantity_on_floor
 
     if apartment_location == 1:
         print(f"Квартира ближняя слева")
