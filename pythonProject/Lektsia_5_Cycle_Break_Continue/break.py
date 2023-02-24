250c3c59f4120d91652d8395ca576888cafeeec1
Anton <a.lyakh@gmail.com>
2023-02-24 18:01:38 +0700

    fix homework numbers prime and break

--- pythonProject/Lektsia_5_Cycle_Break_Continue/break.py
+++ pythonProject/Lektsia_5_Cycle_Break_Continue/break.py
@@ -1,8 +1,10 @@
 fixed_string = "abcd"
 
 while True:
-
     user_string = input("Введите строку: ")
 
     if user_string == fixed_string:
+        print("Вы ввели верную строку")
         break
+    else:
+        print("Вы ввели не верную строку")
