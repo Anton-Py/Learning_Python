c7f1c5f9fe40da3874c5dc08b9725e40bb95b631
Anton <a.lyakh@gmail.com>
2023-02-27 00:47:43 +0700

    add fix in homework next date

--- pythonProject/Lektsia_3_Branching/next_date.py
+++ pythonProject/Lektsia_3_Branching/next_date.py
@@ -1,36 +1,35 @@
-day = int(input("Введите день недели: "))
+day = int(input("Введите номер дня недели на сегодня: "))
 month = int(input("Введите сегодняшний месяц: "))
 year = int(input("Введите сегодняшний год: "))
 
-if month > 12:
+if month > 12 or month <= 0:
     print(f"Количество месяцев в году 12, вы ввели месяц {month}, введите корректную дату.")
-elif len(str(year)) != 4:
-    print(f"Вы ввели не корректный год {year}, введите корректную дату.")
-
-if month == 4 or month == 6 or month == 9 or month == 11:
-    day_in_month = 30
-elif month == 2:
-    if year % 4 != 0 or (year % 100 == 0 and year % 400 != 0):
-        day_in_month = 28
+else:
+    if month == 4 or month == 6 or month == 9 or month == 11:
+        days_quantity_in_month = 30
+    elif month == 2:
+        if year % 4 != 0 or (year % 100 == 0 and year % 400 != 0):
+            days_quantity_in_month = 28
+        else:
+            days_quantity_in_month = 29
     else:
-        day_in_month = 29
-else:
-    day_in_month = 31
+        days_quantity_in_month = 31
 
-if day > day_in_month:
-    print(f"Количество дней в веденном вами месяце {day_in_month}, вы ввели дней {day}, введите корректную дату.")
-
-if day == day_in_month and month == 12:
-    final_day = 1
-    final_month = 1
-    final_year = year + 1
-elif day == day_in_month and month < 12:
-    final_day = 1
-    final_month = month + 1
-    final_year = year
-else:
-    final_day = day + 1
-    final_month = month
-    final_year = year
-
-print(f"{final_day:0>2}.{final_month:0>2}.{final_year}")
+    if day > days_quantity_in_month or day <= 0:
+        print(f"Количество дней в веденном вами месяце {days_quantity_in_month},"
+              f" вы ввели дней {day}, введите корректную дату.")
+    elif day == days_quantity_in_month and month == 12:
+        next_day = 1
+        next_month = 1
+        next_year = year + 1
+        print(f"{next_day:0>2}.{next_month:0>2}.{next_year}")
+    elif day == days_quantity_in_month and month < 12:
+        next_day = 1
+        next_month = month + 1
+        next_year = year
+        print(f"{next_day:0>2}.{next_month:0>2}.{next_year}")
+    else:
+        next_day = day + 1
+        next_month = month
+        next_year = year
+        print(f"{next_day:0>2}.{next_month:0>2}.{next_year}")
