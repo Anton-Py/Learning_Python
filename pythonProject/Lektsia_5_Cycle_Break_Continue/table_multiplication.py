0a376159963879e7fc7baf5ea1c23f7b9e35f9a5
Anton <a.lyakh@gmail.com>
2023-02-27 12:07:13 +0700

    fix homework table multiplication

--- pythonProject/Lektsia_5_Cycle_Break_Continue/table_multiplication.py
+++ pythonProject/Lektsia_5_Cycle_Break_Continue/table_multiplication.py
@@ -1,21 +1,24 @@
-final_number = int(input("Введите конечное число: "))
+end_number = int(input("Введите конечное число: "))
+
+length_end_number_square = len(str(end_number ** 2))
+length_end_number = len(str(end_number))
 
 # отступ в шапке для цифр
-print(end=" " * (len(str(final_number ** 2)) + 1))
+print(end=" " * (length_end_number_square + 1))
 # выводим шапку
-for i in range(1, final_number + 1):
-    print(f"{i: >{(len(str(final_number ** 2))) - 1}}|", end=" ")
+for i in range(1, end_number + 1):
+    print(f"{i: >{length_end_number_square - 1}}|", end=" ")
 print()
 # отступ в шапке для разделителя
-print(end=" " * (len(str(final_number ** 2)) + 1))
+print(end=" " * (length_end_number_square + 1))
 # выводим разделитель
-for i in range(1, final_number + 1):
-    print(f"{'--': >{len(str(final_number ** 2))}}", end=" ")
+for i in range(1, end_number + 1):
+    print(f"{'--': >{length_end_number_square}}", end=" ")
 print()
 # выводим таблицу умножения
-for i in range(1, final_number + 1):
-    print(f"{i: >{len(str(final_number))}}|", end=" ")
-    for j in range(1, final_number + 1):
+for i in range(1, end_number + 1):
+    print(f"{i: >{length_end_number}}|", end=" ")
+    for j in range(1, end_number + 1):
         c = i * j
-        print(f"{c: >{len(str(final_number ** 2))}}", end=" ")
+        print(f"{c: >{length_end_number_square}}", end=" ")
     print()
