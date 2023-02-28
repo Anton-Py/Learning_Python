995c898b88ee78ea610823b0502af54d0abf14f5
Anton <a.lyakh@gmail.com>
2023-03-01 01:54:22 +0700

    add homework reboot

--- pythonProject/Lektsia_6_Functions/reboot.py
+++ pythonProject/Lektsia_6_Functions/reboot.py
@@ -1,27 +1,53 @@
+from multipledispatch import dispatch
+
+
 def get_type_name(data):
     if isinstance(data, str):
         print("Это строка")
-
-
-def get_type_name(data):
     if isinstance(data, int):
         print("Это целое число")
-
-
-def get_type_name(data):
     if isinstance(data, complex):
         print("Это комплексное число")
-
-
-def get_type_name(data):
     if isinstance(data, float):
         print("Это число с плавающей точкой")
-
-
-def get_type_name(data):
     if isinstance(data, bool):
         print("Это булево значение")
 
 
 get_type_name(data="qwerty")
 get_type_name(data=1)
+get_type_name(data=3 + 4j)
+get_type_name(data=1.5)
+get_type_name(data=False)
+
+
+@dispatch(int)
+def get_type_name(data):
+    print("Это целое число")
+
+
+@dispatch(str)
+def get_type_name(data):
+    print("Это строка")
+
+
+@dispatch(float)
+def get_type_name(data):
+    print("Это число с плавающей точкой")
+
+
+@dispatch(complex)
+def get_type_name(data):
+    print("Это комплексное число")
+
+
+@dispatch(bool)
+def get_type_name(data):
+    print("Это булево значение")
+
+
+get_type_name("qwerty")
+get_type_name(1)
+get_type_name(3 + 4j)
+get_type_name(1.5)
+get_type_name(False)
--- pythonProject/Lektsia_6_Functions/temperature_conversion.py
+++ pythonProject/Lektsia_6_Functions/temperature_conversion.py
@@ -1,4 +1,4 @@
-celsius_temperature = int(input("Введите температуру в градусах цельсия: "))
+temperature_celsius = int(input("Введите температуру в градусах цельсия: "))
 
 
 def conversion_temperature_to_fahrenheit(temperature_in_celsius):
@@ -9,8 +9,8 @@ def conversion_temperature_to_kelvin(temperature_in_celsius):
     return temperature_in_celsius + 273.15
 
 
-temperature_in_fahrenheit = conversion_temperature_to_fahrenheit(celsius_temperature)
-temperature_in_kalvin = conversion_temperature_to_kelvin(celsius_temperature)
+temperature_in_fahrenheit = conversion_temperature_to_fahrenheit(temperature_celsius)
+temperature_in_kalvin = conversion_temperature_to_kelvin(temperature_celsius)
 
 print(f"Температура в градусах Фаренгейта равна: {temperature_in_fahrenheit}\n"
       f"температура в градусах Кельвина равна: {temperature_in_kalvin}")
