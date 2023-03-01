727aa96489674af2ad687f9291015767206f023a
Anton <a.lyakh@gmail.com>
2023-03-01 13:16:27 +0700

    fix homework deposit

--- pythonProject/Lektsia_5_Cycle_Break_Continue/deposit.py
+++ pythonProject/Lektsia_5_Cycle_Break_Continue/deposit.py
@@ -1,9 +1,12 @@
-deposit_amount = int(input("Введите сумму вклада: "))
+deposit_amount = float(input("Введите сумму вклада: "))
 contract_period = int(input("Введите срок договора в месяцах: "))
 annual_rate = float(input("Введите размер годовой ставки: "))
 
-total_profit = deposit_amount * pow((1 + annual_rate / 100 / 12), contract_period)
-print(f"Ваш итоговый доход: {total_profit:.2f}")
+NUMBER_FOR_ANNUAL_RATE = 100
+MONTH_QUANTITY = 12
 
-margin = total_profit - deposit_amount
-print(f"Ваша прибыль: {margin:.2f}")
+amount_the_end_period = deposit_amount * pow(1 + annual_rate / NUMBER_FOR_ANNUAL_RATE / MONTH_QUANTITY, contract_period)
+print(f"Ваш итоговый доход: {amount_the_end_period:.2f}")
+
+profit = amount_the_end_period - deposit_amount
+print(f"Ваша прибыль: {profit:.2f}")
