90a64b8662f2bd15e21b5a5f6f246b44c6bd520e
Anton <a.lyakh@gmail.com>
2023-03-03 01:32:33 +0700

    fix homework algoritm_euclidean

--- pythonProject/Lektsia_5_Cycle_Break_Continue/algorithm_euclidean.py
+++ pythonProject/Lektsia_5_Cycle_Break_Continue/algorithm_euclidean.py
@@ -1,12 +1,11 @@
-first_number = int(input("Введите первое число: "))
-second_number = int(input("Введите второе число: "))
+number_1 = int(input("Введите первое число: "))
+number_2 = int(input("Введите второе число: "))
 
-while first_number != 0 and second_number != 0:
-    if first_number > second_number:
-        first_number %= second_number
-    else:
-        second_number %= first_number
+if number_1 == 0 and number_2 == 0:
+    print("Алгоритм искать нельзя")
+elif number_2 == 0:
+    nod = number_1
+while number_2 != 0:
+    number_1, number_2 = number_2, number_1 % number_2
 
-greatest_common_divisor = first_number + second_number
-
-print(f"Наибольший общий делитель = {greatest_common_divisor}")
+print(f"Наибольший общий делитель = {number_1}")
