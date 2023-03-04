8ef942d107ece790bdb2a193e054d2fe6777235b
Anton <a.lyakh@gmail.com>
2023-03-05 01:34:39 +0700

    add fix in test quadratic_equation

--- pythonProject/Lektsia_3_Branching/quadratic_equation.py
+++ pythonProject/Lektsia_3_Branching/quadratic_equation.py
@@ -6,24 +6,24 @@ c = float(input("Введите коэффициент c: "))
 
 epsilon = 0.0001
 
-if abs(a) >= epsilon:  # a != 0
+if abs(a) > epsilon:  # a != 0
 
     discriminant = math.pow(b, 2) - (4 * a * c)
 
-    if discriminant < epsilon:  # d < 0
-        print("Уравнение не имеет корней")
-    elif discriminant > epsilon:  # d > 0
+    if abs(discriminant) > epsilon:  # d > 0
         x_1 = (-b + math.sqrt(discriminant)) / (2 * a)
         x_2 = (-b - math.sqrt(discriminant)) / (2 * a)
         print(f'Уравнение имеет два корня. Корень первый: {x_1:.4f}, корень второй: {x_2:.4f}')
-    else:
+    elif abs(discriminant) <= epsilon:  # d == 0
         x = -b / 2 * a
         print(f"Уравнение имеет один корень. Корень: {x:.4f}")
+    else:  # d < 0
+        print("Уравнение не имеет корней")
 
-elif abs(b) >= epsilon:  # b != 0
-    x = -c / b
+elif abs(b) > epsilon:  # b != 0
+    x = c / b
     print(f"Решением уравнения является: {x:.4f}")
-elif abs(b) <= epsilon and abs(c) >= epsilon:  # b == 0 c != 0
+elif abs(b) <= epsilon < abs(c):  # b == 0 c != 0
     print("Уравнение корней не имеет")
 elif abs(b) <= epsilon and abs(c) <= epsilon:  # b == 0 c == 0
     print("Корнем уравнения является любое число")
