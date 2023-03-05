2132909b3aced5698c76a3a5e63ae1ec3e43fa49
Anton <a.lyakh@gmail.com>
2023-03-05 23:10:25 +0700

    add fix in conctant homework order value

--- pythonProject/Lektsia_6_Functions/order_value.py
+++ pythonProject/Lektsia_6_Functions/order_value.py
@@ -1,19 +1,18 @@
-SMALL_DISCOUNT = 0.05
-BIG_DISCOUNT = 0.10
-PRODUCTS_PRICE_FOR_DISCOUNT = 1000
-PRODUCTS_QUANTITY_FOR_DISCOUNT = 10
-
-
 def get_calculation_order_cost(product_quantity_1, product_quantity_2, product_price_1, product_price_2):
     products_quantity = product_quantity_1 + product_quantity_2
     cash_sum = product_price_1 + product_price_2
 
-    if products_quantity >= PRODUCTS_QUANTITY_FOR_DISCOUNT and cash_sum >= PRODUCTS_PRICE_FOR_DISCOUNT:
-        products_price_including_discount = cash_sum - cash_sum * BIG_DISCOUNT
-    elif products_quantity >= PRODUCTS_QUANTITY_FOR_DISCOUNT:
-        products_price_including_discount = cash_sum - cash_sum * SMALL_DISCOUNT
-    elif cash_sum >= PRODUCTS_PRICE_FOR_DISCOUNT:
-        products_price_including_discount = cash_sum - cash_sum * SMALL_DISCOUNT
+    small_discount = 0.05
+    big_discount = 0.10
+    products_price_for_discount = 1000
+    products_quantity_for_discount = 10
+
+    if products_quantity >= products_quantity_for_discount and cash_sum >= products_price_for_discount:
+        products_price_including_discount = cash_sum - cash_sum * big_discount
+    elif products_quantity >= products_quantity_for_discount:
+        products_price_including_discount = cash_sum - cash_sum * small_discount
+    elif cash_sum >= products_price_for_discount:
+        products_price_including_discount = cash_sum - cash_sum * small_discount
     else:
         products_price_including_discount = cash_sum
 
