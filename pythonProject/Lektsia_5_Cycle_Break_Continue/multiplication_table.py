8f88278cd0b672b9a6e949bd171a2e6286730da8
Anton <a.lyakh@gmail.com>
2023-03-07 18:04:12 +0700

    add fix in test homework multiplication_table

--- pythonProject/Lektsia_5_Cycle_Break_Continue/multiplication_table.py
+++ pythonProject/Lektsia_5_Cycle_Break_Continue/multiplication_table.py
@@ -4,19 +4,26 @@ end_number_square_length = len(str(end_number ** 2))
 end_number_length = len(str(end_number))
 
 # отступ в шапке для цифр
-print(end=" " * (end_number_square_length + 2))
+if end_number_square_length < 2:
+    print(end=" " * (end_number_square_length + 3))
+else:
+    print(end=" " * (end_number_square_length + 2))
 
 # выводим шапку
 for i in range(1, end_number + 1):
-    print(f"{i: >{end_number_square_length - 1}}|", end=" ")
+    print(f"{i: >{end_number_square_length}}|", end=" ")
 
 print()
+
 # отступ в шапке для разделителя
-print(end=" " * (end_number_square_length + 1))
+if end_number_square_length < 2:
+    print(end=" " * (end_number_square_length + 2))
+else:
+    print(end=" " * (end_number_square_length + 1))
 
 # выводим разделитель
 for i in range(1, end_number + 1):
-    print(f"{'--': >{end_number_square_length}}", end=" ")
+    print(f"{'--': >{end_number_square_length + 1}}", end=" ")
 
 print()
 
@@ -25,7 +32,7 @@ for i in range(1, end_number + 1):
     print(f"{i: >{end_number_length}}|", end=" ")
 
     for j in range(1, end_number + 1):
-        Internal_number = i * j
-        print(f"{Internal_number: >{end_number_square_length}}", end=" ")
+        numbers_product = i * j
+        print(f"{numbers_product: >{end_number_square_length + 1}}", end=" ")
 
     print()
