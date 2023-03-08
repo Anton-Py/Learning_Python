2fda87fbaa21b9d4f2db93ec609b2842db23aa8c
Anton <a.lyakh@gmail.com>
2023-03-09 02:04:29 +0700

    add fix in homework order_cost

---
+++ pythonProject/Lektsia_6_Functions/order_cost.py
@@ -0,0 +1,25 @@
+def get_order_cost(product_1_quantity, product_1_price, product_2_quantity, product_2_price):
+    products_quantity = product_1_quantity + product_2_quantity
+    order_cost = product_1_price + product_2_price
+
+    small_discount = 0.05
+    big_discount = 0.10
+    products_cost_for_discount = 1000
+    products_quantity_for_discount = 10
+
+    if products_quantity >= products_quantity_for_discount and order_cost >= products_cost_for_discount:
+        products_cost_including_discount = order_cost - order_cost * big_discount
+    elif products_quantity >= products_quantity_for_discount:
+        products_cost_including_discount = order_cost - order_cost * small_discount
+    elif order_cost >= products_cost_for_discount:
+        products_cost_including_discount = order_cost - order_cost * small_discount
+    else:
+        products_cost_including_discount = order_cost
+
+    return products_cost_including_discount
+
+
+print("Стоимость заказа равна:", get_order_cost(3, 500, 5, 800))
+print("Стоимость заказа равна:", get_order_cost(4, 600, 7, 500))
+print("Стоимость заказа равна:", get_order_cost(4, 300, 6, 200))
+print("Стоимость заказа равна:", get_order_cost(4, 300, 2, 300))
