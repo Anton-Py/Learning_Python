7656e4780a372a3b321435ac992b564c6094c8c5
Anton <a.lyakh@gmail.com>
2023-03-08 22:13:57 +0700

    add fix in homework temperature_conversion

--- pythonProject/Lektsia_6_Functions/temperature_conversion.py
+++ pythonProject/Lektsia_6_Functions/temperature_conversion.py
@@ -1,16 +1,15 @@
+def get_temperature_in_fahrenheit(temperature_in_celsius):
+    return 9 / 5 * temperature_in_celsius + 32
+
+
+def get_temperature_in_kelvin(temperature_in_celsius):
+    return temperature_in_celsius + 273.15
+
+
 temperature_celsius = float(input("Введите температуру в градусах цельсия: "))
 
-
-def get_conversion_temperature_to_fahrenheit(temperature):
-    return 9 / 5 * temperature + 32
-
-
-def get_conversion_temperature_to_kelvin(temperature):
-    return temperature + 273.15
-
-
-temperature_in_fahrenheit = get_conversion_temperature_to_fahrenheit(temperature_celsius)
-temperature_in_kelvin = get_conversion_temperature_to_kelvin(temperature_celsius)
+temperature_in_fahrenheit = get_temperature_in_fahrenheit(temperature_celsius)
+temperature_in_kelvin = get_temperature_in_kelvin(temperature_celsius)
 
 print(f"Температура в градусах Фаренгейта равна: {temperature_in_fahrenheit}\n"
       f"температура в градусах Кельвина равна: {temperature_in_kelvin}")
