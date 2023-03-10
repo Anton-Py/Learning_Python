142a8db10ac4852445d7c8c3bc0fa0b73d9bf4bf
Anton <a.lyakh@gmail.com>
2023-03-10 21:14:58 +0700

    add fux in homework order_cost

--- pythonProject/Lektsia_6_Functions/order_cost.py
+++ pythonProject/Lektsia_6_Functions/order_cost.py
@@ -8,18 +8,18 @@ def get_order_cost(product_1_quantity, product_1_price, product_2_quantity, prod
     products_quantity_for_discount = 10
 
     if products_quantity >= products_quantity_for_discount and order_cost >= products_cost_for_discount:
-        products_cost_including_discount = order_cost - order_cost * big_discount
+        orders_cost_including_discount = order_cost - order_cost * big_discount
     elif products_quantity >= products_quantity_for_discount:
-        products_cost_including_discount = order_cost - order_cost * small_discount
+        orders_cost_including_discount = order_cost - order_cost * small_discount
     elif order_cost >= products_cost_for_discount:
-        products_cost_including_discount = order_cost - order_cost * small_discount
+        orders_cost_including_discount = order_cost - order_cost * small_discount
     else:
-        products_cost_including_discount = order_cost
+        orders_cost_including_discount = order_cost
 
-    return products_cost_including_discount
+    return orders_cost_including_discount
 
 
-print("Стоимость заказа равна:", get_order_cost(3, 500, 5, 800))
+print("Стоимость заказа равна:", get_order_cost(3, 700, 5, 500))
 print("Стоимость заказа равна:", get_order_cost(4, 600, 7, 500))
 print("Стоимость заказа равна:", get_order_cost(4, 300, 6, 200))
-print("Стоимость заказа равна:", get_order_cost(4, 300, 2, 300))
+print("Стоимость заказа равна:", get_order_cost(1, 300, 2, 300))
