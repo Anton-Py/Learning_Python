7f81960bb4945048a21cbb8f4444c3a225317d50
Anton <a.lyakh@gmail.com>
2023-03-10 21:01:54 +0700

    add fix in homework order_cost

--- pythonProject/Lektsia_5_Cycle_Break_Continue/deposit.py
+++ pythonProject/Lektsia_5_Cycle_Break_Continue/deposit.py
@@ -2,12 +2,12 @@ deposit_amount = float(input("Введите сумму вклада: "))
 contract_period = int(input("Введите срок договора в месяцах: "))
 annual_rate = float(input("Введите размер годовой ставки: "))
 
-ONE_HUNDRED_PERCENT = 100
+HUNDRED_PERCENT = 100
 MONTHS_QUANTITY_IN_YEAR = 12
 
-deposit_on_amount_period_end = deposit_amount * pow(
-    1 + annual_rate / ONE_HUNDRED_PERCENT / MONTHS_QUANTITY_IN_YEAR, contract_period)
-print(f"Ваш итоговый доход: {deposit_on_amount_period_end:.2f}")
+deposit_amount_at_period_end = deposit_amount * pow(
+    1 + annual_rate / HUNDRED_PERCENT / MONTHS_QUANTITY_IN_YEAR, contract_period)
+print(f"Ваш итоговый доход: {deposit_amount_at_period_end:.2f}")
 
-profit = deposit_on_amount_period_end - deposit_amount
+profit = deposit_amount_at_period_end - deposit_amount
 print(f"Ваша прибыль: {profit:.2f}")
