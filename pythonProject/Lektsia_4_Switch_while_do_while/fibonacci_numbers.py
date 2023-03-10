2e933e4c0b41e2496f934ba43cda7a2b2137dcb1
Anton <a.lyakh@gmail.com>
2023-03-10 20:40:18 +0700

    git add fix in homework fibonacci_numbers

--- pythonProject/Lektsia_4_Switch_while_do_while/fibonacci_numbers.py
+++ pythonProject/Lektsia_4_Switch_while_do_while/fibonacci_numbers.py
@@ -1,15 +1,15 @@
 fibonacci_number_index = int(input("Введите номер элемента ряда Фибоначчи: "))
 
-fibonacci_previous_number = 0
-fibonacci_next_number = 1
+previous_fibonacci_number = 0
+current_fibonacci_number = 1
 
 i = 0
 
 while i < fibonacci_number_index:
-    fibonacci_numbers_sum = fibonacci_previous_number + fibonacci_next_number
-    fibonacci_previous_number = fibonacci_next_number
-    fibonacci_next_number = fibonacci_numbers_sum
+    fibonacci_numbers_sum = previous_fibonacci_number + current_fibonacci_number
+    previous_fibonacci_number = current_fibonacci_number
+    current_fibonacci_number = fibonacci_numbers_sum
 
     i += 1
 
-print(f"Число Фибоначчи с номером {fibonacci_number_index} =", fibonacci_previous_number)
+print(f"Число Фибоначчи с номером {fibonacci_number_index} =", previous_fibonacci_number)
