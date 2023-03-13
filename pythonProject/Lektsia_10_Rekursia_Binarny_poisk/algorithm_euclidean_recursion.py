0f888e4d23c62d7be18076b3ed9f6e06effc48bf
Anton <a.lyakh@gmail.com>
2023-03-13 23:02:38 +0700

    add fix in homework algoritm_euclidian_recursion

--- pythonProject/Lektsia_10_Rekursia_Binarny_poisk/algorithm_euclidean_recursion.py
+++ pythonProject/Lektsia_10_Rekursia_Binarny_poisk/algorithm_euclidean_recursion.py
@@ -1,14 +1,14 @@
 def get_greatest_common_divisor(number_1, number_2):
     if number_1 == 0 and number_2 == 0:
-        print("Наибольший общий делитель искать нельзя")
+        return "Наибольший общий делитель искать нельзя"
     elif number_2 == 0:
-        greatest_common_divisor = number_1
+        return number_1
     else:
         while number_2 != 0:
-            number_1, number_2 = number_2, number_1 % number_2
-    return number_1
+            return get_greatest_common_divisor(number_2, number_1 % number_2)
 
 
 user_number_1 = int(input("Введите первое число: "))
 user_number_2 = int(input("Введите второе число: "))
+
 print(f"Наибольший общий делитель = {get_greatest_common_divisor(user_number_1, user_number_2)}")
