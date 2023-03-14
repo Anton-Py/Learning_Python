161580f7486007f5547fff487ae1af78b31af271
Anton <a.lyakh@gmail.com>
2023-03-15 01:46:16 +0700

    add fix in homewoek sorting_verification

---
+++ pythonProject/Lektsia_9_Spiski_Otladka_Argumenty_programmy/sorting_verification.py
@@ -0,0 +1,25 @@
+def is_list_sorted_by_ascending(list_to_verification):
+    for i in range(len(list_to_verification) - 1):
+        if list_to_verification[i] < list_to_verification[i + 1] \
+                or list_to_verification[i] == list_to_verification[i + 1]:
+            continue
+        else:
+            return False
+    return True
+
+
+def is_list_sorted_by_descending(list_to_verification):
+    for i in range(len(list_to_verification) - 1):
+        if list_to_verification[i] > list_to_verification[i + 1] \
+                or list_to_verification[i] == list_to_verification[i + 1]:
+            continue
+        else:
+            return False
+    return True
+
+
+entered_list_to_verification = [int(i) for i in
+                                input("Введите несколько чисел через пробел для создания списка: ").split()]
+
+print(is_list_sorted_by_ascending(entered_list_to_verification))
+print(is_list_sorted_by_descending(entered_list_to_verification))
