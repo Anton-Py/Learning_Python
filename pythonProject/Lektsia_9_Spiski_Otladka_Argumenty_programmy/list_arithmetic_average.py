fbe7459860d1bf43793dac4370a28a74d0a82d1a
Anton <a.lyakh@gmail.com>
2023-03-14 22:52:58 +0700

    add fix in homework

--- pythonProject/Lektsia_9_Spiski_Otladka_Argumenty_programmy/list_arithmetic_average.py
+++ pythonProject/Lektsia_9_Spiski_Otladka_Argumenty_programmy/list_arithmetic_average.py
@@ -1,19 +1,17 @@
 def get_even_numbers_arithmetic_average(numbers_list):
-    prime_numbers_sum = 0
-    prime_numbers_quantity = 0
+    numbers_sum = 0
+    numbers_quantity = 0
 
-    for i in numbers_list:
-        if i % 2 == 0:
-            prime_numbers_sum += i
-            prime_numbers_quantity += 1
+    for number in numbers_list:
+        if number % 2 == 0:
+            numbers_sum += number
+            numbers_quantity += 1
 
-    even_numbers_arithmetic_average = prime_numbers_sum / prime_numbers_quantity
-
-    return even_numbers_arithmetic_average
+    return numbers_sum / numbers_quantity
 
 
 entered_numbers_list = [int(i) for i in
-                        input("Введите несколько числел через пробел, для создания списка: ").split()]
+                        input("Введите несколько чисел через пробел, для создания списка: ").split()]
 
 print("Среднее арифметическое элементов списка, которые являются четными числами:",
       get_even_numbers_arithmetic_average(entered_numbers_list))
