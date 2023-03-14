2ddcdfc0583fe248c3d73d3389de62a2205cb902
Anton <a.lyakh@gmail.com>
2023-03-14 23:16:52 +0700

    add fix in homework quadratic_education

--- pythonProject/Lektsia_3_Branching/quadratic_equation.py
+++ pythonProject/Lektsia_3_Branching/quadratic_equation.py
@@ -14,15 +14,15 @@ if abs(a) > epsilon:  # a != 0
         x_2 = (-b - math.sqrt(discriminant)) / (2 * a)
         print(f'Уравнение имеет два корня. Корень первый: {x_1:.4f}, корень второй: {x_2:.4f}')
     elif abs(discriminant) <= epsilon:  # d == 0
-        x = -b / 2 * a
+        x = -b / (2 * a)
         print(f"Уравнение имеет один корень. Корень: {x:.4f}")
     else:  # d < 0
         print("Уравнение не имеет корней")
 
 elif abs(b) > epsilon:  # b != 0
-    x = - c / b
+    x = -c / b
     print(f"Решением уравнения является: {x:.4f}")
-elif abs(b) <= epsilon and abs(c) <= epsilon:  # b == 0 c == 0
+elif abs(c) <= epsilon:  # b == 0 c == 0
     print("Корнем уравнения является любое число")
 else:  # b == 0 c != 0
     print("Уравнение корней не имеет")
