fcabdaf13c9ea884b3aa5abaaf3b80c2179b5df6
Anton <a.lyakh@gmail.com>
2023-03-14 16:16:14 +0700

    add fix in homework element_search

--- pythonProject/Lektsia_9_Spiski_Otladka_Argumenty_programmy/element_search.py
+++ pythonProject/Lektsia_9_Spiski_Otladka_Argumenty_programmy/element_search.py
@@ -1,12 +1,12 @@
-def search_element_in_list(list_to_search, umber_to_search):
+def get_element_index(list_to_search, number_to_search):
     for i in range(len(list_to_search)):
-        if int(list_to_search[i]) == umber_to_search:
+        if int(list_to_search[i]) == number_to_search:
             return i
-    else:
-        return -1
+
+    return -1
 
 
-entered_list_to_search = input("Введите несколько числел через пробел, для создания списка: ").split()
+entered_list_to_search = [int(i) for i in input("Введите несколько чисел через пробел, для создания списка:").split()]
 entered_number_to_search = int(input("Введите число для поиска в созданном вами списке: "))
 
-print(search_element_in_list(entered_list_to_search, entered_number_to_search))
+print(get_element_index(entered_list_to_search, entered_number_to_search))
