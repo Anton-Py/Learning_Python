5979baaf028f8e567ee951d5d21f3a61089debeb
Anton <a.lyakh@gmail.com>
2023-03-14 21:26:07 +0700

    add fix in homework array_reversal

---
+++ pythonProject/Lektsia_9_Spiski_Otladka_Argumenty_programmy/array_reversal.py
@@ -0,0 +1,8 @@
+def reversal_list(list_for_reversal):
+    for i in range(1, len(list_for_reversal) // 2 + 1):
+        list_for_reversal[i - 1], list_for_reversal[-i] = list_for_reversal[-i], list_for_reversal[i - 1]
+
+
+entered_list_for_reversal = input("Введите данные через пробел, для формирования списка: ").split()
+
+reversal_list(entered_list_for_reversal)
