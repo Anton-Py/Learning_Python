d1680dc824bb501cdc80f6808055a6dcc1bf9d9c
Anton <a.lyakh@gmail.com>
2023-03-21 12:51:27 +0700

    add fix in homewrok binart_search

--- pythonProject/Lektsia_10_Rekursia_Binarny_poisk/binary_search.py
+++ pythonProject/Lektsia_10_Rekursia_Binarny_poisk/binary_search.py
@@ -1,11 +1,13 @@
-def binary_search(lst, value):
+def binary_search(list_for_search, value):
     left = 0
-    right = len(lst) - 1
+    right = len(list_for_search) - 1
     middle = (left + right) // 2
 
-    while lst[middle] != value and left <= right:
+    if len(list_for_search) == 0:
+        return -1
 
-        if lst[middle] < value:
+    while list_for_search[middle] != value and left <= right:
+        if list_for_search[middle] < value:
             left = middle + 1
         else:
             right = middle - 1
@@ -14,29 +16,31 @@ def binary_search(lst, value):
 
     if left > right:
         return -1
-    else:
-        return middle
+
+    return middle
 
 
-def binary_search_with_recursion(lst, value, left, right):
+def binary_search_with_recursion(list_for_search, value, left, right):
     if left > right:
         return -1
     else:
         middle = (left + right) // 2
 
-        if lst[middle] == value:
+        if list_for_search[middle] == value:
             return middle
-        elif lst[middle] < value:
-            return binary_search_with_recursion(lst, value, middle + 1, right)
+        elif list_for_search[middle] < value:
+            return binary_search_with_recursion(list_for_search, value, middle + 1, right)
         else:
-            return binary_search_with_recursion(lst, value, left, middle - 1)
+            return binary_search_with_recursion(list_for_search, value, left, middle - 1)
 
 
-list_for_search = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-value_for_search = 11
+# user_list_for_search = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# user_list_for_search = [8]
+user_list_for_search = []
+user_value_for_search = 8
 
 left_index = 0
-right_index = len(list_for_search) - 1
+right_index = len(user_list_for_search) - 1
 
-print(binary_search(list_for_search, value_for_search))
-print(binary_search_with_recursion(list_for_search, value_for_search, left_index, right_index))
+print(binary_search(user_list_for_search, user_value_for_search))
+print(binary_search_with_recursion(user_list_for_search, user_value_for_search, left_index, right_index))
