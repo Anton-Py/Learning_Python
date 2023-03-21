68248944ab3e4b0170ee5366551243968a5ce567
Anton <a.lyakh@gmail.com>
2023-03-21 12:34:25 +0700

    add fix in homework raising to power

--- pythonProject/Lektsia_10_Rekursia_Binarny_poisk/raising_to_power.py
+++ pythonProject/Lektsia_10_Rekursia_Binarny_poisk/raising_to_power.py
@@ -1,11 +1,11 @@
-def raising_to_power_with_recursion(number, number_degree):
+def get_number_to_power_with_recursion(number, number_degree):
     if number_degree == 0:
         return 1
 
-    return number * raising_to_power_with_recursion(number, number_degree - 1)
+    return number * get_number_to_power_with_recursion(number, number_degree - 1)
 
 
-def raising_to_power(number, number_degree):
+def get_number_to_power(number, number_degree):
     numbers_product = 1
 
     for i in range(number_degree):
@@ -17,5 +17,6 @@ def raising_to_power(number, number_degree):
 user_number = int(input("Введите число: "))
 user_number_degree = int(input("Введите степень числа: "))
 
-print(raising_to_power_with_recursion(user_number, user_number_degree))
-print(raising_to_power(user_number, user_number_degree))
+print(f"Число {user_number} в степени {user_number_degree} равно",
+      get_number_to_power_with_recursion(user_number, user_number_degree))
+print(f"Число {user_number} в степени {user_number_degree} равно", get_number_to_power(user_number, user_number_degree))
