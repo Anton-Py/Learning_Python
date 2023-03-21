5578cc5bb6a9bf3b1b56c69a6f0a8f6eeb0f947e
Anton <a.lyakh@gmail.com>
2023-03-21 20:21:23 +0700

    add fix in homework

--- pythonProject/Lektsia_10_Rekursia_Binarny_poisk/raising_to_power.py
+++ pythonProject/Lektsia_10_Rekursia_Binarny_poisk/raising_to_power.py
@@ -1,22 +1,23 @@
-def get_number_to_power_with_recursion(number, number_degree):
-    if number_degree == 0:
+def get_number_in_power_with_recursion(number, number_to_raise_in_power):
+    if number_to_raise_in_power == 0:
         return 1
 
-    return number * get_number_to_power_with_recursion(number, number_degree - 1)
+    return number * get_number_in_power_with_recursion(number, number_to_raise_in_power - 1)
 
 
-def get_number_to_power(number, number_degree):
+def get_number_in_power(number, number_to_raise_in_power):
     numbers_product = 1
 
-    for i in range(number_degree):
+    for i in range(number_to_raise_in_power):
         numbers_product *= number
 
     return numbers_product
 
 
 user_number = int(input("Введите число: "))
-user_number_degree = int(input("Введите степень числа: "))
+user_number_to_raise_in_power = int(input("Введите степень числа: "))
 
-print(f"Число {user_number} в степени {user_number_degree} равно",
-      get_number_to_power_with_recursion(user_number, user_number_degree))
-print(f"Число {user_number} в степени {user_number_degree} равно", get_number_to_power(user_number, user_number_degree))
+print(f"Число {user_number} в степени {user_number_to_raise_in_power} равно",
+      get_number_in_power_with_recursion(user_number, user_number_to_raise_in_power))
+print(f"Число {user_number} в степени {user_number_to_raise_in_power} равно",
+      get_number_in_power(user_number, user_number_to_raise_in_power))
---
+++ pythonProject/Lektsia_11_Python_Sortirovki_Bystraya_sortirovka/heapsort.py
@@ -0,0 +1,9 @@
+def heapsort(numbers_list):
+    pass
+
+
+list_for_sort = [5, 9, 3, 4, 1, 6, 2, 8]
+
+print(heapsort(list_for_sort))
+
+
