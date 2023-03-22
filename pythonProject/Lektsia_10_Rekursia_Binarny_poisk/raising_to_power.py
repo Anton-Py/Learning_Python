f9cb04b4dcf955b22cf67760ac70b8e5e3f2c355
Anton <a.lyakh@gmail.com>
2023-03-22 17:10:10 +0700

    add fix in homework raising_to_power

--- pythonProject/Lektsia_10_Rekursia_Binarny_poisk/raising_to_power.py
+++ pythonProject/Lektsia_10_Rekursia_Binarny_poisk/raising_to_power.py
@@ -1,8 +1,8 @@
-def get_number_in_power_with_recursion(number, number_to_raise_in_power):
-    if number_to_raise_in_power == 0:
+def get_number_in_power_with_recursion(number, exponent):
+    if exponent == 0:
         return 1
 
-    return number * get_number_in_power_with_recursion(number, number_to_raise_in_power - 1)
+    return number * get_number_in_power_with_recursion(number, exponent - 1)
 
 
 def get_number_in_power(number, number_to_raise_in_power):
@@ -15,9 +15,9 @@ def get_number_in_power(number, number_to_raise_in_power):
 
 
 user_number = int(input("Введите число: "))
-user_number_to_raise_in_power = int(input("Введите степень числа: "))
+user_exponent = int(input("Введите степень числа: "))
 
-print(f"Число {user_number} в степени {user_number_to_raise_in_power} равно",
-      get_number_in_power_with_recursion(user_number, user_number_to_raise_in_power))
-print(f"Число {user_number} в степени {user_number_to_raise_in_power} равно",
-      get_number_in_power(user_number, user_number_to_raise_in_power))
+print(f"Число {user_number} в степени {user_exponent} равно",
+      get_number_in_power_with_recursion(user_number, user_exponent))
+print(f"Число {user_number} в степени {user_exponent} равно",
+      get_number_in_power(user_number, user_exponent))
