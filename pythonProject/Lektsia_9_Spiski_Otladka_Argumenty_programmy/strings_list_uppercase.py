bbb15c17f15cb7c8a6ea7ed36669fda6bd59804d
Anton <a.lyakh@gmail.com>
2023-03-22 13:02:15 +0700

    add fix in homework strings_list_uppercase

--- pythonProject/Lektsia_9_Spiski_Otladka_Argumenty_programmy/strings_list_uppercase.py
+++ pythonProject/Lektsia_9_Spiski_Otladka_Argumenty_programmy/strings_list_uppercase.py
@@ -2,9 +2,7 @@ def convert_list_to_uppercase(strings_list):
     for i in range(len(strings_list)):
         strings_list[i] = strings_list[i].upper()
 
-    return strings_list
-
 
 entered_strings_list = input("Введите несколько строк через пробел для создания списка: ").split()
 
-print(convert_list_to_uppercase(entered_strings_list))
+convert_list_to_uppercase(entered_strings_list)
