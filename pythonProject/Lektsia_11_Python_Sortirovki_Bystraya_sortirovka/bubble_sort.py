47b26689b46f033a0485b36b40fed93406b0b8cd
Anton <a.lyakh@gmail.com>
2023-03-23 16:22:47 +0700

    add fix in homework bubble_sort

--- pythonProject/Lektsia_11_Python_Sortirovki_Bystraya_sortirovka/bubble_sort.py
+++ pythonProject/Lektsia_11_Python_Sortirovki_Bystraya_sortirovka/bubble_sort.py
@@ -1,20 +1,19 @@
-def run_bubble_sort(items):
-    for limit in range(len(items) - 1, -1, -1):
+def is_bubble_sort(items):
+    for limit in range(len(items) - 1, 0, -1):
         swapped = False
-        
+
         for i in range(limit):
             if items[i] > items[i + 1]:
                 temp = items[i]
                 items[i] = items[i + 1]
                 items[i + 1] = temp
+
                 swapped = True
 
         if not swapped:
             break
 
-    return items
 
+list_for_sort = [5, 9, 3, 4, 1, 6, 2, 8, 10, 3, 16]
 
-list_for_sort = [5, 9, 3, 4, 1, 6, 2, 8]
-
-print(run_bubble_sort(list_for_sort))
+is_bubble_sort(list_for_sort)
