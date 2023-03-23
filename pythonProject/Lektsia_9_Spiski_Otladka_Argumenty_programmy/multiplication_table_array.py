b1305bd1fc1b6d40bfa40b8977810905d4dbdd8c
Anton <a.lyakh@gmail.com>
2023-03-23 15:28:25 +0700

    add fix

--- pythonProject/Lektsia_9_Spiski_Otladka_Argumenty_programmy/multiplication_table_array.py
+++ pythonProject/Lektsia_9_Spiski_Otladka_Argumenty_programmy/multiplication_table_array.py
@@ -2,7 +2,7 @@ def get_multiplication_table(table_size):
     multiplication_table = []
 
     for i in range(1, table_size + 1):
-        nested_array = [0 for _ in range(1, table_size + 1)]
+        nested_array = [0] * table_size
         multiplication_table.append(nested_array)
 
     for i in range(table_size):
@@ -13,8 +13,11 @@ def get_multiplication_table(table_size):
 
 
 user_table_size = int(input("Введите число, которое задает размер таблицы: "))
+end_number = len(str(user_table_size ** 2))
 
 multiplication_table_list = get_multiplication_table(user_table_size)
 
 for row in multiplication_table_list:
-    print(*row)
+    for symbol in row:
+        print(f"{symbol: >{end_number}}", end=" ")
+    print()
