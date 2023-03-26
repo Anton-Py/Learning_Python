74472b053ed90655e588e14478d5b46ac22d8168
Anton <a.lyakh@gmail.com>
2023-03-26 21:58:32 +0700

    fix homework binary_search

--- pythonProject/Lektsia_10_Rekursia_Binarny_poisk/binary_search.py
+++ pythonProject/Lektsia_10_Rekursia_Binarny_poisk/binary_search.py
@@ -1,43 +1,42 @@
 def binary_search(list_for_search, value):
-    left = 0
-    right = len(list_for_search) - 1
-    middle = (left + right) // 2
-
     if len(list_for_search) == 0:
         return -1
 
-    while list_for_search[middle] != value and left <= right:
+    left = 0
+    right = len(list_for_search) - 1
+
+    while left <= right:
+        middle = (left + right) // 2
+
+        if list_for_search[middle] == value:
+            return middle
+
         if list_for_search[middle] < value:
             left = middle + 1
         else:
             right = middle - 1
 
-        middle = (left + right) // 2
-
-    if left > right:
-        return -1
-
-    return middle
+    return -1
 
 
 def binary_search_with_recursion(list_for_search, value, left, right):
     if left > right:
         return -1
-    else:
-        middle = (left + right) // 2
 
-        if list_for_search[middle] == value:
-            return middle
-        elif list_for_search[middle] < value:
-            return binary_search_with_recursion(list_for_search, value, middle + 1, right)
-        else:
-            return binary_search_with_recursion(list_for_search, value, left, middle - 1)
+    middle = (left + right) // 2
+
+    if list_for_search[middle] == value:
+        return middle
+    elif list_for_search[middle] < value:
+        return binary_search_with_recursion(list_for_search, value, middle + 1, right)
+    else:
+        return binary_search_with_recursion(list_for_search, value, left, middle - 1)
 
 
 # user_list_for_search = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# user_list_for_search = [8]
-user_list_for_search = []
-user_value_for_search = 8
+user_list_for_search = [2, 8, 5]
+# user_list_for_search = []
+user_value_for_search = 4
 
 left_index = 0
 right_index = len(user_list_for_search) - 1
