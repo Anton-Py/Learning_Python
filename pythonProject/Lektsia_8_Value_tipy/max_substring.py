38c757959e2ac0b1cbe4c76c80526ea60f2a8a10
Anton <a.lyakh@gmail.com>
2023-03-26 22:15:25 +0700

    add fix in homework max substring

--- pythonProject/Lektsia_8_Value_tipy/max_substring.py
+++ pythonProject/Lektsia_8_Value_tipy/max_substring.py
@@ -3,20 +3,20 @@ def get_max_substring_length(string):
         return 0
 
     string = string.lower()
-    max_value_for_substring = 1
-    current_value_for_substring = 1
+    max_length_for_substring = 1
+    current_length_for_substring = 1
 
     for i in range(0, len(string) - 1):
         if string[i] == string[i + 1]:
-            current_value_for_substring += 1
+            current_length_for_substring += 1
 
-            if current_value_for_substring > max_value_for_substring:
-                max_value_for_substring = current_value_for_substring
+            if current_length_for_substring > max_length_for_substring:
+                max_length_for_substring = current_length_for_substring
 
         else:
-            current_value_for_substring = 1
+            current_length_for_substring = 1
 
-    return max_value_for_substring
+    return max_length_for_substring
 
 
 user_string = input("Введите строку: ")
