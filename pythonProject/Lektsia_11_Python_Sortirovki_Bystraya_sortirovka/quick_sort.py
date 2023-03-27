408f4c8f83d3235196c67b0b40a0fda9d9ea8436
Anton <a.lyakh@gmail.com>
2023-03-28 01:51:19 +0700

    add fix in homework quick_sort

--- pythonProject/Lektsia_11_Python_Sortirovki_Bystraya_sortirovka/quick_sort.py
+++ pythonProject/Lektsia_11_Python_Sortirovki_Bystraya_sortirovka/quick_sort.py
@@ -1,33 +1,30 @@
-import random
-
-
-def quick_sort(numbers_list, left, right):
+def quick_sort(list_for_sort, left, right):
     if left >= right:
         return
 
-    random_list_element_on_segment = numbers_list[random.randint(left, right)]
+    support_member = list_for_sort[left]
     i = left
     j = right
 
     while i <= j:
-        while numbers_list[i] < random_list_element_on_segment:
+        while list_for_sort[i] < support_member:
             i += 1
 
-        while numbers_list[j] > random_list_element_on_segment:
+        while list_for_sort[j] > support_member:
             j -= 1
 
         if i <= j:
-            temp = numbers_list[i]
-            numbers_list[i] = numbers_list[j]
-            numbers_list[j] = temp
+            temp = list_for_sort[i]
+            list_for_sort[i] = list_for_sort[j]
+            list_for_sort[j] = temp
             i += 1
             j -= 1
 
-            quick_sort(numbers_list, left, j)
-            quick_sort(numbers_list, i, right)
+    quick_sort(list_for_sort, left, j)
+    quick_sort(list_for_sort, i, right)
 
 
-list_for_sort = [5, 9, 3, 4, 1, 6, 2, 8]
+user_list_for_sort = [5, 9, 3, 4, 1, 6, 2, 8]
 
-quick_sort(list_for_sort, 0, len(list_for_sort) - 1)
-print(list_for_sort)
+quick_sort(user_list_for_sort, 0, len(user_list_for_sort) - 1)
+print(user_list_for_sort)
