cdc97e70e566827b28c202729ba3b13dd2af0498
Anton <a.lyakh@gmail.com>
2023-03-28 23:04:13 +0700

    add new homework

--- pythonProject/Lektsia_12_Python_Rabota_s_faylami_i_strokami/quantity_occurrences.py
+++ pythonProject/Lektsia_12_Python_Rabota_s_faylami_i_strokami/quantity_occurrences.py
@@ -1,7 +1,26 @@
-def get_quantity_occurrences():
-    with open("data/input_data.txt", "r", newline="\r") as input_file:
-        for line in input_file.readlines():
-            pass
+def get_quantity_occurrences(text, string_to_search, start_index):
+    string_with_text = text.lower()
+    start = -1
+    count = 0
+
+    while True:
+        start = string_with_text.find(string_to_search, start + start_index)
+
+        if start == -1:
+            break
+
+        count += 1
+
+    return count
 
 
-get_quantity_occurrences()
+def get_text_from_file():
+    with open("data/input_data.txt", "r", newline="\r", encoding="utf-8") as input_file:
+        return input_file.read()
+
+
+text_from_file = get_text_from_file()
+user_string_to_search = "файл"
+user_start_index = 4
+
+print("Число вхождений:", get_quantity_occurrences(text_from_file, user_string_to_search, user_start_index))
