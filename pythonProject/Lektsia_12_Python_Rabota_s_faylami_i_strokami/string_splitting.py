8bd6a0b70919837c99e83a4c69c057838d867338
Anton <a.lyakh@gmail.com>
2023-03-31 21:53:13 +0700

    add fix in homework string_splitting

--- pythonProject/Lektsia_12_Python_Rabota_s_faylami_i_strokami/string_splitting.py
+++ pythonProject/Lektsia_12_Python_Rabota_s_faylami_i_strokami/string_splitting.py
@@ -1,16 +1,18 @@
-def get_numbers_sum_in_string(string):
-    string_list = string.split(", ")
+def numbers_sum_in_string(string):
+    strings_list = string.split(", ")
     numbers_sum = 0
     numbers_list = []
 
-    for i in string_list:
+    for i in strings_list:
         number = int(i)
         numbers_sum += number
         numbers_list.append(number)
 
-    print("Список из чисел строки:", numbers_list, "\n" "Cумма чисел строки:", numbers_sum)
+    return numbers_list, numbers_sum
 
 
 user_string = "5, 9, 3, 4, 1, 6, 2, 8, 10, 3, 16"
 
-get_numbers_sum_in_string(user_string)
+data = numbers_sum_in_string(user_string)
+
+print("Список из чисел строки:", data[0], "\n" "Сумма чисел строки:", data[1])
