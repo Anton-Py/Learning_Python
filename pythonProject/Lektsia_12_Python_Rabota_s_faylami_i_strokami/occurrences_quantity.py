6579dd39d8a795f47d7c0bc6a337110cb2861668
Anton <a.lyakh@gmail.com>
2023-04-01 21:45:25 +0700

    add fix in homework occurrences_quantity

--- pythonProject/Lektsia_12_Python_Rabota_s_faylami_i_strokami/occurrences_quantity.py
+++ pythonProject/Lektsia_12_Python_Rabota_s_faylami_i_strokami/occurrences_quantity.py
@@ -1,12 +1,13 @@
-def get_occurrences_quantity(text, string_to_search, start_index):
+def get_occurrences_quantity(text, string_to_search):
     string_with_text = text.lower()
-    start = -1
+    string_search = string_to_search.lower()
+    start_index = -1
     quantity = 0
 
     while True:
-        start = string_with_text.find(string_to_search, start + start_index)
+        start_index = string_with_text.find(string_search, start_index + 1)
 
-        if start == -1:
+        if start_index == -1:
             break
 
         quantity += 1
@@ -15,12 +16,13 @@ def get_occurrences_quantity(text, string_to_search, start_index):
 
 
 def get_text_from_file():
-    with open("data/input_data.txt", "r", newline="\r\n", encoding="utf-8") as input_file:
-        return input_file.read()
+    with open("data/input_data.txt", "r", newline="\n", encoding="utf-8") as input_file:
+        array = [row.strip() for row in input_file]
+        return " ".join(array)
 
 
 text_from_file = get_text_from_file()
-user_string_to_search = "файл"
-user_start_index = 4
 
-print("Число вхождений:", get_occurrences_quantity(text_from_file, user_string_to_search, user_start_index))
+user_string_to_search = "Файл"
+
+print("Число вхождений:", get_occurrences_quantity(text_from_file, user_string_to_search))
