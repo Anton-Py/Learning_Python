3669d9b0d0db347827790683eae75ee2328c79e0
Anton <a.lyakh@gmail.com>
2023-04-01 12:29:41 +0700

    add fix in homework

--- pythonProject/Lektsia_12_Python_Rabota_s_faylami_i_strokami/convert_file_to_uppercase.py
+++ pythonProject/Lektsia_12_Python_Rabota_s_faylami_i_strokami/convert_file_to_uppercase.py
@@ -2,7 +2,6 @@ def convert_file_to_uppercase():
     with open("data/input_data.txt", "r", newline="\r\n", encoding="utf-8") as input_file, \
             open("data/output.txt", "w") as output_file:
         for line in input_file.readlines():
-            print(line)
             output_file.write(line.upper())
 
 
--- pythonProject/Lektsia_12_Python_Rabota_s_faylami_i_strokami/occurrences_quantity.py
+++ pythonProject/Lektsia_12_Python_Rabota_s_faylami_i_strokami/occurrences_quantity.py
@@ -1,7 +1,7 @@
-def get_quantity_occurrences(text, string_to_search, start_index):
+def get_occurrences_quantity(text, string_to_search, start_index):
     string_with_text = text.lower()
     start = -1
-    count = 0
+    quantity = 0
 
     while True:
         start = string_with_text.find(string_to_search, start + start_index)
@@ -9,13 +9,13 @@ def get_quantity_occurrences(text, string_to_search, start_index):
         if start == -1:
             break
 
-        count += 1
+        quantity += 1
 
-    return count
+    return quantity
 
 
 def get_text_from_file():
-    with open("data/input_data.txt", "r", newline="\r", encoding="utf-8") as input_file:
+    with open("data/input_data.txt", "r", newline="\r\n", encoding="utf-8") as input_file:
         return input_file.read()
 
 
@@ -23,4 +23,4 @@ text_from_file = get_text_from_file()
 user_string_to_search = "файл"
 user_start_index = 4
 
-print("Число вхождений:", get_quantity_occurrences(text_from_file, user_string_to_search, user_start_index))
+print("Число вхождений:", get_occurrences_quantity(text_from_file, user_string_to_search, user_start_index))
