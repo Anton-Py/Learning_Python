e5c5b9a1d39000a830e209428c5eea29e1131703
Anton <a.lyakh@gmail.com>
2023-04-05 15:14:04 +0700

    add fix in homework convert file to upper case

--- pythonProject/Lektsia_12_Python_Rabota_s_faylami_i_strokami/convert_file_to_uppercase.py
+++ pythonProject/Lektsia_12_Python_Rabota_s_faylami_i_strokami/convert_file_to_uppercase.py
@@ -1,8 +1,12 @@
-def convert_file_to_uppercase():
-    with open("data/input_data.txt", "r", newline="\r\n", encoding="utf-8") as input_file, \
-            open("data/output.txt", "w") as output_file:
-        for line in input_file.readlines():
-            output_file.write(line.upper())
+def convert_file_to_uppercase(path_read, path_write):
+    with open(path_read, "r", encoding="utf-8") as input_file, \
+            open(path_write, "w", newline="\n", encoding="utf-8") as output_file:
+        for line in input_file:
+            if line.strip():
+                output_file.write(line.upper())
 
 
-convert_file_to_uppercase()
+path_file_read = "data/input_data.txt"
+path_file_write = "data/output.txt"
+
+convert_file_to_uppercase(path_file_read, path_file_write)
