cfb9356d361fcdd56b8ac40461f4ca464f13faa5
Anton <a.lyakh@gmail.com>
2023-04-09 14:44:58 +0700

    add fix in homework max_substring

--- pythonProject/Lektsia_12_Python_Rabota_s_faylami_i_strokami/convert_file_to_uppercase.py
+++ pythonProject/Lektsia_12_Python_Rabota_s_faylami_i_strokami/convert_file_to_uppercase.py
@@ -1,12 +1,11 @@
-def convert_file_to_uppercase(path_read, path_write):
-    with open(path_read, "r", encoding="utf-8") as input_file, \
-            open(path_write, "w", newline="\n", encoding="utf-8") as output_file:
+def convert_file_to_uppercase(file_path, path_write):
+    with open(file_path, "r", encoding="utf-8") as input_file, \
+            open(path_write, "w", encoding="utf-8") as output_file:
         for line in input_file:
-            if line.strip():
-                output_file.write(line.upper())
+            output_file.write(line.upper())
 
 
-path_file_read = "data/input_data.txt"
+input_file_path = "data/input_data.txt"
 path_file_write = "data/output.txt"
 
-convert_file_to_uppercase(path_file_read, path_file_write)
+convert_file_to_uppercase(input_file_path, path_file_write)
