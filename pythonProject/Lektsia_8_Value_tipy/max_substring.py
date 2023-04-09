a4ca6e320999cc4c341179c79cc1eb0aa0d14118
Anton <a.lyakh@gmail.com>
2023-04-09 14:22:41 +0700

    git fix in homework max_substring

--- pythonProject/Lektsia_8_Value_tipy/max_substring.py
+++ pythonProject/Lektsia_8_Value_tipy/max_substring.py
@@ -1,24 +1,24 @@
-def get_substring_max_length(string):
+def get_max_substring_length(string):
     if len(string) == 0:
         return 0
 
     string = string.lower()
-    substring_max_length = 1
-    substring_current_length = 1
+    max_substring_length = 1
+    current_substring_length = 1
 
     for i in range(0, len(string) - 1):
         if string[i] == string[i + 1]:
-            substring_current_length += 1
+            current_substring_length += 1
 
-            if substring_current_length > substring_max_length:
-                substring_max_length = substring_current_length
+            if current_substring_length > max_substring_length:
+                max_substring_length = current_substring_length
 
         else:
-            substring_current_length = 1
+            current_substring_length = 1
 
-    return substring_max_length
+    return max_substring_length
 
 
 user_string = input("Введите строку: ")
 
-print("Длина максимальной подстроки:", get_substring_max_length(user_string))
+print("Длина максимальной подстроки:", get_max_substring_length(user_string))
