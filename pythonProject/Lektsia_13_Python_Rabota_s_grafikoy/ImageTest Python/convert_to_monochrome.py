c57d387f3656a129068072afd88352385b2dfdb2
Anton <a.lyakh@gmail.com>
2023-04-09 20:09:36 +0700

    add fix in homework conver to monochrome

--- pythonProject/Lektsia_13_Python_Rabota_s_grafikoy/ImageTest Python/convert_to_monochrome.py
+++ pythonProject/Lektsia_13_Python_Rabota_s_grafikoy/ImageTest Python/convert_to_monochrome.py
@@ -2,13 +2,15 @@ from PIL import Image
 
 image = Image.open("../image.jpg")
 pixels = image.load()
+
 MAX_RGB = 255
+
 width, height = image.size
 
 for x in range(width):
     for y in range(height):
         pixel = pixels[x, y]
-        index_rgb = (int(0.3 * pixel[0]) + int(0.59 * pixel[1]) + int(0.11 * pixel[2]))
-        pixels[x, y] = (index_rgb, index_rgb, index_rgb)
+        component = round(0.3 * pixel[0] + 0.59 * pixel[1] + 0.11 * pixel[2])
+        pixels[x, y] = (component, component, component)
 
 image.save("out.png")
