782c54b9f42d857c882ec35a3e07137b500c5231
Anton <a.lyakh@gmail.com>
2023-04-10 22:21:59 +0700

    add fix in homework convert_to_monochrome

--- pythonProject/Lektsia_13_Python_Rabota_s_grafikoy/ImageTest Python/convert_to_monochrome.py
+++ pythonProject/Lektsia_13_Python_Rabota_s_grafikoy/ImageTest Python/convert_to_monochrome.py
@@ -10,7 +10,7 @@ width, height = image.size
 for x in range(width):
     for y in range(height):
         pixel = pixels[x, y]
-        component = round(0.3 * pixel[0] + 0.59 * pixel[1] + 0.11 * pixel[2])
-        pixels[x, y] = (component, component, component)
+        grey_color = round(0.3 * pixel[0] + 0.59 * pixel[1] + 0.11 * pixel[2])
+        pixels[x, y] = (grey_color, grey_color, grey_color)
 
 image.save("out.png")
--- pythonProject/Lektsia_13_Python_Rabota_s_grafikoy/ImageTest Python/home_2.py
+++ pythonProject/Lektsia_13_Python_Rabota_s_grafikoy/ImageTest Python/home_2.py
@@ -1,16 +1,19 @@
 from PIL import Image
 
 image = Image.open("../image.jpg")
-pixels = image.load()
+# pixels = image.load()
+#
+# MAX_RGB = 255
+#
+# width, height = image.size
+#
+# for x in range(width):
+#     for y in range(height):
+#         pixel = pixels[x, y]
+#         component = round(0.3 * pixel[0] + 0.59 * pixel[1] + 0.11 * pixel[2])
+#         pixels[x, y] = (component, component, component)
+#
+# image.save("out.png")
 
-MAX_RGB = 255
+r, g, b = image.split()
 
-width, height = image.size
-
-for x in range(width):
-    for y in range(height):
-        pixel = pixels[x, y]
-        component = round(0.3 * pixel[0] + 0.59 * pixel[1] + 0.11 * pixel[2])
-        pixels[x, y] = (component, component, component)
-
-image.save("out.png")
