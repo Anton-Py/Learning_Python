83e2cc1de86ead6e3944ed2051c8e4019fb6d887
Anton <a.lyakh@gmail.com>
2023-04-15 23:47:38 +0700

    add fix homework occurrences_quntity

--- pythonProject/Lektsia_12_Python_Rabota_s_faylami_i_strokami/occurrences_quantity.py
+++ pythonProject/Lektsia_12_Python_Rabota_s_faylami_i_strokami/occurrences_quantity.py
@@ -1,32 +1,32 @@
-def get_occurrences_quantity(text, string_search):
+def get_occurrences_quantity(text, string):
     text = text.lower()
-    string_search = string_search.lower()
+    string = string.lower()
     start_index = 0
     quantity = 0
 
-    index = text.find(string_search, start_index)
+    index = text.find(string, start_index)
 
     while index != -1:
-        start_index = index + 1
+        start_index = index + len(string)
         quantity += 1
 
-        index = text.find(string_search, start_index)
+        index = text.find(string, start_index)
 
     return quantity
 
 
-def get_total_occurrences_quantity(input_file_path, string_to_search):
+def get_total_occurrences_quantity(input_file_path, search_string):
     quantity = 0
 
     with open(input_file_path, "r", encoding="utf-8") as input_file:
         for line in input_file:
-            quantity += get_occurrences_quantity(line, string_to_search)
+            quantity += get_occurrences_quantity(line, search_string)
 
     return quantity
 
 
 user_input_file_path = "data/input_data.txt"
-user_string_to_search = "with"
+user_search_string = "можно"
 
-print("Число вхождений:", get_total_occurrences_quantity(user_input_file_path, user_string_to_search))
+print("Число вхождений:", get_total_occurrences_quantity(user_input_file_path, user_search_string))
 
