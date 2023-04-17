b9831fed5ce896061122aecef97bc06f3caa86ef
Anton <a.lyakh@gmail.com>
2023-04-17 20:36:28 +0700

    add fix in homework heap_sort

--- pythonProject/Lektsia_11_Python_Sortirovki_Bystraya_sortirovka/heap_sort.py
+++ pythonProject/Lektsia_11_Python_Sortirovki_Bystraya_sortirovka/heap_sort.py
@@ -1,7 +1,5 @@
 def sieve(list_for_sort, heap_size, root_index):
-    done = False
-
-    while not done:
+    while True:
         max_index = root_index
         left_child_index = (2 * root_index) + 1
         right_child_index = (2 * root_index) + 2
@@ -12,17 +10,17 @@ def sieve(list_for_sort, heap_size, root_index):
         if right_child_index < heap_size and list_for_sort[right_child_index] > list_for_sort[max_index]:
             max_index = right_child_index
 
-        if max_index != root_index:
-            list_for_sort[root_index], list_for_sort[max_index] = list_for_sort[max_index], list_for_sort[root_index]
-            root_index = max_index
-        else:
-            done = True
+        if max_index == root_index:
+            return
+
+        list_for_sort[root_index], list_for_sort[max_index] = list_for_sort[max_index], list_for_sort[root_index]
+        root_index = max_index
 
 
 def heap_sort(list_for_sort):
     list_length = len(list_for_sort)
 
-    for i in range(list_length, -1, -1):
+    for i in range((list_length // 2) - 1, -1, -1):
         sieve(list_for_sort, list_length, i)
 
     for i in range(list_length - 1, 0, -1):
@@ -30,7 +28,7 @@ def heap_sort(list_for_sort):
         sieve(list_for_sort, i, 0)
 
 
-user_list_for_sort = [5, 9, 3, 4, 1, 6, 2, 8]
+user_list_for_sort = [15, 5, 11, 9, 12, 10, 3, 16, 4, 14, 1, 6, 2, 18, 8, 13]
 
 heap_sort(user_list_for_sort)
 
